88b0f2998b9a3a555e5b8001f7b7175bfe0718c1
Muskan Patel <57407831+muskan7860@users.noreply.github.com>
2026-07-01 21:24:28 +0530

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,3 +1,3 @@
 def call(String name) {
-    echo " Hello , $(name)! This message came from the shared library."
+    echo " Hello , ${name}! This message came from the shared library."
     echo "Current build number: ${env.Build_NUMBER}"
